fc7cf6797440ad639e01999702f0e8657f8d7718
lion92 <38532569+lion92@users.noreply.github.com>
2024-10-31 18:25:49 +0100

    cors

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import * as cors from 'cors';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: console,
   });
+  app.use(cors());
+
   app.use(cookieParser());
   app.enableCors({
     allowedHeaders: '*',
     origin: '*',
     credentials: true,
   });
+
   await app.listen(3012);
 }
 bootstrap();
