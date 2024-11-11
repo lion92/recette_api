e5bcf60ed6216b2388c2cd00f448d6c52432834b
lion92 <38532569+lion92@users.noreply.github.com>
2024-11-11 16:14:49 +0100

    ok

--- src/main.ts
+++ src/main.ts
@@ -2,26 +2,24 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import * as cors from 'cors';
-import * as express from 'express';
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: console,
   });
-
-  // Configurer la limite de taille de la charge utile
-  app.use(express.json({ limit: '50mb' }));
-  app.use(express.urlencoded({ limit: '50mb', extended: true }));
-
   app.use(cors());
+
   app.use(cookieParser());
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
+    credentials: true,
+  });
 
   // Configurer CORS
   app.enableCors({
-    origin: ['https://recette.krissclotilde.com', 'https://www.recette.krissclotilde.com', 'http://localhost:3007'],
+    origin: ['https://recette.krissclotilde.com', 'https://www.recette.krissclotilde.com'],
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
-    credentials: true,
   });
 
   await app.listen(3007);
